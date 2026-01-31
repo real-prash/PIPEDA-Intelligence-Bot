2622659e956f260766ddc1c98fa635dc292d2cd7
Prashant Nigam <nigamp@myumanitoba.ca>
2026-01-30 20:23:45 -0600

    Project setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pipeda_intelligence_bot", # Changed from medical_chatbot
+    version="0.1.0",
+    author="Prashant Nigam",
+    packages=find_packages(),
+    install_requires=[]
+)
